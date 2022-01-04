5c731a5e7aad90dcea2444a08791f0cb1efc21a9
Gianluca Cioni <gianlucacioni93@gmail.com>
2022-01-04 23:21:46 +0100

    Removed " type="submit" "

--- src/components/forms/Button.js
+++ src/components/forms/Button.js
@@ -3,11 +3,7 @@ import React, { Component } from 'react';
 class Button extends Component {
   render() {
     const { text, onClick } = this.props;
-    return (
-      <button onClick={onClick} type="submit">
-        {text}
-      </button>
-    );
+    return <button onClick={onClick}>{text}</button>;
   }
 }
 
