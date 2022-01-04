532a36c6669a47729349a7aa5c846b9e5509ac32
Gianluca Cioni <gianlucacioni93@gmail.com>
2022-01-04 18:23:16 +0100

    extra inputs render correctly

--- src/components/forms/Form.js
+++ src/components/forms/Form.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import Input from './Input';
 import Button from './Button';
+import uniqid from 'uniqid';
 
 class Form extends Component {
   constructor(props) {
     super(props);
-    this.state = this.props.formData;
+    this.state = {
+      ...this.props.formData,
+      extraInputCounter: [uniqid(), uniqid()],
+    };
     this.onInputChange = this.onInputChange.bind(this);
   }
 
@@ -19,6 +23,60 @@ class Form extends Component {
             ...state.formFields[field],
             inputValue: e.target.value,
           },
+        },
+      };
+    });
+  }
+
+  componentDidMount() {
+    let inputName;
+    let inputPlaceholder;
+    let inputType;
+    switch (!inputName && !inputPlaceholder && !inputType) {
+      case this.state.formType === 'workExperiences':
+        inputName = 'workDuty';
+        inputPlaceholder = 'Work Duty';
+        inputType = 'textarea';
+        break;
+      case this.state.formType === 'skills':
+        inputName = 'skill';
+        inputPlaceholder = 'Skill';
+        inputType = 'input';
+        break;
+      case this.state.formType === 'certificates':
+        inputName = 'certificate';
+        inputPlaceholder = 'Certificate';
+        inputType = 'input';
+        break;
+      default:
+        inputName = '';
+        inputPlaceholder = '';
+        inputType = '';
+        break;
+    }
+    const extraInputs = this.state.extraInputCounter.reduce(
+      (accumulator, id) => {
+        accumulator = {
+          ...accumulator,
+          [`${inputName}-${id}`]: {
+            id,
+            inputValue: '',
+            name: `${inputName}-${id}`,
+            placeHolder: inputPlaceholder,
+            type: inputType,
+          },
+        };
+        return accumulator;
+      },
+      {},
+    );
+
+    this.setState((state) => {
+      return {
+        ...state,
+        formFields: {
+          ...state.formFields,
+          ...extraInputs,
         },
       };
     });
