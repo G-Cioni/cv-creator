d30fe4a300e653688e722277c73ed05d68fbd17f
Gianluca Cioni <gianlucacioni93@gmail.com>
2022-01-04 01:32:06 +0100

    added counters for workExperiences and education

--- src/components/forms/Forms.js
+++ src/components/forms/Forms.js
@@ -1,8 +1,35 @@
 import React, { Component } from 'react';
 import Form from './Form';
 import formData from '../../formData';
+import uniqid from 'uniqid';
 
 class Forms extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      workExperiencesCounter: [],
+      educationCounter: [],
+    };
+  }
+
+  componentDidMount() {
+    let updatedState = this.state;
+    if (this.state.workExperiencesCounter.length < 1) {
+      updatedState = {
+        ...updatedState,
+        workExperiencesCounter: updatedState.workExperiencesCounter.concat(
+          uniqid(),
+        ),
+      };
+    }
+    if (this.state.educationCounter.length < 1) {
+      updatedState = {
+        ...updatedState,
+        educationCounter: updatedState.educationCounter.concat(uniqid()),
+      };
+    }
+    this.setState(updatedState);
+  }
   render() {
     const { personalInfo, workExperiences, education } = formData;
     return (
