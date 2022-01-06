bf73448205f58b8f207cd64d8d8aa30400166ec5
Gianluca Cioni <gianlucacioni93@gmail.com>
2022-01-06 03:04:41 +0100

    renamed className

--- src/components/cv/Cv.js
+++ src/components/cv/Cv.js
@@ -5,7 +5,7 @@ const Cv = ({ allFormsData }) => {
   const cards = allFormsData
     ? Object.keys(allFormsData).map((card) => {
         const id = card.substring(card.indexOf('-') + 1);
-        const className = card.substring(0, card.indexOf('-'));
+        const className = card.substring(0, card.indexOf('-')) + 'Card';
         return (
           <Card key={id} className={className} formData={allFormsData[card]} />
         );
