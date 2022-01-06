1df2097ed05afd5421ce76e7dfbb2645b7e53579
Gianluca Cioni <gianlucacioni93@gmail.com>
2022-01-06 02:50:58 +0100

    Card and Cv  are now stateless functional comps

--- src/components/cv/Card.js
+++ src/components/cv/Card.js
@@ -1,22 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Card extends Component {
-  render() {
-    const { formData } = this.props;
-    const details = formData
-      ? Object.keys(formData).map((detail) => {
-          const { inputValue: text, id, name: className } = formData[detail];
-          //todo consider rendering different html tags instead of always a div
-          return (
-            <div key={id} className={className}>
-              {text}
-            </div>
-          );
-        })
-      : null;
-
-    return <div className="card">{details}</div>;
-  }
-}
+const Card = ({ formData }) => {
+  const details = formData
+    ? Object.keys(formData).map((detail) => {
+        const { inputValue: text, id, name: className } = formData[detail];
+        //todo consider rendering different html tags instead of always a div
+        return (
+          <div key={id} className={className}>
+            {text}
+          </div>
+        );
+      })
+    : null;
+  return <div className="card">{details}</div>;
+};
 
 export default Card;
--- src/components/cv/Cv.js
+++ src/components/cv/Cv.js
@@ -1,17 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Card from './Card';
 
-class Cv extends Component {
-  render() {
-    const { allFormsData } = this.props;
-    const cards = allFormsData
-      ? Object.keys(allFormsData).map((card) => {
-          const id = card.substring(card.indexOf('-') + 1);
-          return <Card key={id} formData={allFormsData[card]} />;
-        })
-      : null;
-    return <div id="cv">{cards}</div>;
-  }
-}
+const Cv = ({ allFormsData }) => {
+  const cards = allFormsData
+    ? Object.keys(allFormsData).map((card) => {
+        const id = card.substring(card.indexOf('-') + 1);
+        return <Card key={id} formData={allFormsData[card]} />;
+      })
+    : null;
+  return <div id="cv">{cards}</div>;
+};
 
 export default Cv;
