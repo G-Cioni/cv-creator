b4edd1e72d70ef6fb9b74cc124b6076da9f4510c
Gianluca Cioni <gianlucacioni93@gmail.com>
2022-01-06 02:58:27 +0100

    className is assigned correctly for extraInputs

--- src/components/cv/Card.js
+++ src/components/cv/Card.js
@@ -3,7 +3,10 @@ import React from 'react';
 const Card = ({ formData }) => {
   const details = formData
     ? Object.keys(formData).map((detail) => {
-        const { inputValue: text, id, name: className } = formData[detail];
+        const { inputValue: text, id, name } = formData[detail];
+        const className = name.includes('-')
+          ? name.substring(0, name.indexOf('-'))
+          : name;
         //todo consider rendering different html tags instead of always a div
         return (
           <div key={id} className={className}>
