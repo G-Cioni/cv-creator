62cc054fb8b2a191482c6f940b5fb59c8309efbc
Gianluca Cioni <gianlucacioni93@gmail.com>
2022-01-08 01:23:47 +0100

    Cv title now render conditionally

--- src/components/cv/Cv.jsx
+++ src/components/cv/Cv.jsx
@@ -7,12 +7,29 @@ const Cv = ({ allFormsData }) => {
         const id = card.substring(card.indexOf('-') + 1);
         const cardArray = card.substring(0, card.indexOf('-'));
         const className = cardArray + 'Card';
+
+        //Checks if any input in the formData has an input value. Returns a boolean
+        let valuePresent = false;
+
+        Object.keys(allFormsData[card]).forEach((inputName) => {
+          Object.keys(allFormsData[card][inputName]).forEach(
+            (inputProperty) => {
+              if (inputProperty === 'inputValue') {
+                valuePresent =
+                  valuePresent ||
+                  allFormsData[card][inputName][inputProperty] !== '';
+              }
+            },
+          );
+        });
+
         if (accumulator[cardArray] === undefined) {
           accumulator[cardArray] = [];
         }
         accumulator[cardArray].push(
           <Card key={id} className={className} formData={allFormsData[card]} />,
         );
+        accumulator[`${cardArray}HasValue`] = valuePresent;
         return accumulator;
       }, {})
     : null;
@@ -21,10 +38,15 @@ const Cv = ({ allFormsData }) => {
     personalInfo,
     contact,
     workExperiences,
+    workExperiencesHasValue,
     education,
+    educationHasValue,
     skills,
+    skillsHasValue,
     certificates,
+    certificatesHasValue,
     languages,
+    languagesHasValue,
   } = cards;
   return (
     <div id="Cv">
@@ -35,21 +57,23 @@ const Cv = ({ allFormsData }) => {
       <div id="workEducation">
         <div id="workExperiencesCV">
           <h2 className="cvTitle">
-            {workExperiences ? 'Work Experience' : null}
+            {workExperiencesHasValue ? 'Work Experience' : null}
           </h2>
           {workExperiences}
         </div>
         <div id="educationCv">
-          <h2 className="cvTitle">{education ? 'Education' : null}</h2>
+          <h2 className="cvTitle">{educationHasValue ? 'Education' : null}</h2>
           {education}
         </div>
       </div>
       <div id="scl">
-        <h2 className="cvTitle">{skills ? 'Skills' : null}</h2>
+        <h2 className="cvTitle">{skillsHasValue ? 'Skills' : null}</h2>
         {skills}
-        <h2 className="cvTitle">{certificates ? 'Certificates' : null}</h2>
+        <h2 className="cvTitle">
+          {certificatesHasValue ? 'Certificates' : null}
+        </h2>
         {certificates}
-        <h2 className="cvTitle">{languages ? 'Languages' : null}</h2>
+        <h2 className="cvTitle">{languagesHasValue ? 'Languages' : null}</h2>
         {languages}
       </div>
     </div>
