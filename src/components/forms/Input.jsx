f6e46662fab7e5cedcd80f2b4b0e4f8b8ae09c69
Gianluca Cioni <gianlucacioni93@gmail.com>
2022-01-18 01:22:12 +0100

    Remove button now appears next to extraInputs

--- src/components/forms/Input.jsx
+++ src/components/forms/Input.jsx
@@ -15,9 +15,13 @@ class Input extends Component {
       type,
     } = this.props;
 
+    const moreThanOneExtraInput = removeExtraInput && counter.length > 1;
+    const extraInputClassName = moreThanOneExtraInput
+      ? 'inputWithRemoveBtn'
+      : null;
     // Conditionally renders an input field or a textarea
     return (
-      <div>
+      <div className={extraInputClassName}>
         {type === 'input' ? (
           <input
             onChange={(e) => handleOnChange(e, name)}
@@ -31,11 +35,8 @@ class Input extends Component {
             value={inputValue}
           />
         )}
-        {removeExtraInput && counter.length > 1 ? (
-          <Button
-            onClick={() => removeExtraInput(name, formName)}
-            text={`Remove ${extraInputName}`}
-          />
+        {moreThanOneExtraInput ? (
+          <Button onClick={() => removeExtraInput(name, formName)} text={`X`} />
         ) : null}
       </div>
     );
