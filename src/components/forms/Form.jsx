38285161508a3afb2bca0ae14196cc7b5b2939da
Gianluca Cioni <gianlucacioni93@gmail.com>
2022-01-20 23:53:42 +0100

    Form state is saved to local storage

--- src/components/forms/Form.jsx
+++ src/components/forms/Form.jsx
@@ -7,17 +7,22 @@ import uniqid from 'uniqid';
 class Form extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      ...this.props.formData,
-      extraInputsCounter: [],
-    };
+    const { formId } = this.props;
+    localStorage[`cvCreator${formId}`] =
+      localStorage[`cvCreator${formId}`] ??
+      JSON.stringify({
+        ...this.props.formData,
+        extraInputsCounter: [],
+      });
+    this.state = JSON.parse(localStorage[`cvCreator${formId}`]);
+
     this.addExtraInput = this.addExtraInput.bind(this);
     this.removeExtraInput = this.removeExtraInput.bind(this);
   }
 
   // Adds an extra input and takes into consideration it's context
   addExtraInput() {
-    const { hasExtraInputs } = this.props;
+    const { formId, hasExtraInputs } = this.props;
 
     let {
       extraInputsCounter,
@@ -25,63 +30,73 @@ class Form extends Component {
       extraInputPlaceHolder,
       extraInputName,
     } = this.state;
-    const newCounter = extraInputsCounter.concat(uniqid());
 
-    if (hasExtraInputs) {
-      let extraInputs = [];
+    //todo can probably remove this next line after local storage maintenance is implemented
+    if (extraInputsCounter) {
+      const newCounter = extraInputsCounter.concat(uniqid());
 
-      // Creates a new object which temporarily holds the new state for each new input field
-      extraInputs = newCounter.reduce((accumulator, id) => {
-        //Checks if the extra input is already in state and updates the accumulator if it isn't
-        if (!extraInputsCounter.includes(id)) {
-          accumulator = {
-            ...accumulator,
-            [`${extraInputName}-${id}`]: {
-              id,
-              inputValue: '',
-              name: `${extraInputName}-${id}`,
-              placeHolder: extraInputPlaceHolder,
-              type: extraInputType,
-            },
-          };
-        }
-        return accumulator;
-      }, {});
+      if (hasExtraInputs) {
+        let extraInputs = [];
 
-      // Adds all of the extra input fields to the state and updates the counter
-      this.setState((state) => {
-        return {
-          ...state,
+        // Creates a new object which temporarily holds the new state for each new input field
+        extraInputs = newCounter.reduce((accumulator, id) => {
+          //Checks if the extra input is already in state and updates the accumulator if it isn't
+          if (!extraInputsCounter.includes(id)) {
+            accumulator = {
+              ...accumulator,
+              [`${extraInputName}-${id}`]: {
+                id,
+                inputValue: '',
+                name: `${extraInputName}-${id}`,
+                placeHolder: extraInputPlaceHolder,
+                type: extraInputType,
+              },
+            };
+          }
+          return accumulator;
+        }, {});
+
+        // Adds all of the extra input fields to the state and updates the counter
+        const updatedState = {
+          ...this.state,
           formFields: {
-            ...state.formFields,
+            ...this.state.formFields,
             ...extraInputs,
           },
           extraInputsCounter: newCounter,
         };
-      });
+        this.setState(updatedState);
+        localStorage[`cvCreator${formId}`] = JSON.stringify(updatedState);
+      }
     }
   }
 
   // Removes a specific input field from the state and counter
   removeExtraInput(name, formName) {
     const { extraInputsCounter } = this.state;
+    const { formId } = this.props;
+
+    let updatedState;
     if (extraInputsCounter.length > 1) {
-      this.setState((state) => {
-        delete state.formFields[name];
-        return {
-          ...state,
-          extraInputsCounter: state.extraInputsCounter.filter(
-            (id) => !name.includes(id),
-          ),
-        };
-      });
+      delete this.state.formFields[name];
+      updatedState = {
+        ...this.state,
+        extraInputsCounter: this.state.extraInputsCounter.filter(
+          (id) => !name.includes(id),
+        ),
+      };
+      this.setState(updatedState);
+      localStorage[`cvCreator${formId}`] = JSON.stringify(updatedState);
     }
     this.props.deleteInputState(formName, name);
   }
 
   componentDidMount() {
-    // Add's the first uniqid to the extraInputCounter
-    this.addExtraInput();
+    // Add's the first uniqid to the extraInputCounter if it's empty
+    const { extraInputsCounter } = this.state;
+    if (extraInputsCounter.length < 1) {
+      this.addExtraInput();
+    }
   }
 
   render() {
@@ -103,38 +118,46 @@ class Form extends Component {
     } = this.props;
 
     // Creates an array of all the input fields to then render
-    const inputs = Object.keys(formFields).map((inputName) => {
-      const { id: defaultId, placeHolder, name, type } = formFields[inputName];
-      const formName = getName(formType, formId);
-      const inputId = name.includes(defaultId) ? name : getName(name, formId);
+    let inputs;
+    if (formFields) {
+      inputs = Object.keys(formFields).map((inputName) => {
+        const {
+          id: defaultId,
+          placeHolder,
+          name,
+          type,
+        } = formFields[inputName];
+        const formName = getName(formType, formId);
+        const inputId = name.includes(defaultId) ? name : getName(name, formId);
 
-      return (
-        <Input
-          counter={extraInputsCounter}
-          extraInputName={extraInputName ?? null}
-          formName={formName}
-          handleOnChange={(e) =>
-            onInputChange(e, formType, formId, formFields, name)
-          }
-          inputId={inputId}
-          inputValue={
-            this.props.appState[formName] &&
-            this.props.appState[formName][inputName] &&
-            this.props.appState[formName][inputName].inputValue
-              ? this.props.appState[formName][inputName].inputValue
-              : ''
-          }
-          key={inputId}
-          name={name}
-          parentFormType={formType}
-          placeHolder={placeHolder}
-          removeExtraInput={
-            name.includes(inputId) ? this.removeExtraInput : null
-          }
-          type={type}
-        />
-      );
-    });
+        return (
+          <Input
+            counter={extraInputsCounter}
+            extraInputName={extraInputName ?? null}
+            formName={formName}
+            handleOnChange={(e) =>
+              onInputChange(e, formType, formId, formFields, name)
+            }
+            inputId={inputId}
+            inputValue={
+              this.props.appState[formName] &&
+              this.props.appState[formName][inputName] &&
+              this.props.appState[formName][inputName].inputValue
+                ? this.props.appState[formName][inputName].inputValue
+                : ''
+            }
+            key={inputId}
+            name={name}
+            parentFormType={formType}
+            placeHolder={placeHolder}
+            removeExtraInput={
+              name.includes(inputId) ? this.removeExtraInput : null
+            }
+            type={type}
+          />
+        );
+      });
+    }
 
     return (
       <div className="form">
